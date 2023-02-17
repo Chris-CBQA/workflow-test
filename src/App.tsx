ac7d6e421a202c97281044d3daac59470fa6bd06
Chris-CBQA <christopher@cbqasolutions.com>
2023-02-16 19:23:16 -0600

    update: lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Comp from './components/Comp';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -16,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Comp content="SomeStuf" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Comp.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  content: string;
+};
+
+const Comp = ({ content }: Props) => {
+  return <div>Dummy Component: {content}</div>;
+};
+
+export default Comp;
